82735f00be04da151ba790a3f359f8667c621dd7
Vince Yuan <vince.yuan@gmail.com>
2015-02-04 15:50:00 +0800

    Initial Commit

---
+++ WatchTicTacToe/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  WatchTicTacToe
+//
+//  Created by Vince Yuan on 4/2/15.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ WatchTicTacToe/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  WatchTicTacToe
+//
+//  Created by Vince Yuan on 4/2/15.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
